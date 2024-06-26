e79987e949581eb4d56640bc9f3596f81c7cb3ab
IAmHim89 <computertech0418@gmail.com>
2024-06-26 17:50:53 -0400

    Assignment Completed

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
 async function render(fi, si) {
-    let response = await fetch(`https://rickandmortyapi.com/api/character/${fi},${si}`)
-    let data = await response.json()
-    console.log(data)
-    // Your code goes here
+  let response = await fetch(
+    `https://rickandmortyapi.com/api/character/${fi},${si}`
+  );
+  let data = await response.json();
+  console.log(data);
+  // Your code goes here
+  //queryselector for both images
+  let firstImage = document.querySelector("#firstImage");
+  let secondImage = document.querySelector("#secondImage");
+
+  firstImage.src = "https://rickandmortyapi.com/api/character/avatar/181.jpeg";
+  secondImage.src = "https://rickandmortyapi.com/api/character/avatar/532.jpeg";
 }
 
 render(20, 35);
